684a81239f34614fca2272c9b1c457d308756c50
Ján Kľuka <jan.kluka@fmph.uniba.sk>
2021-01-12 00:38:10 +0100

    Oprava generovania sha1 sum

--- Aplikácia/include/sha1sums.php
+++ Aplikácia/include/sha1sums.php
@@ -1,4 +1,4 @@
 <?php $sha1sums = array(
 	"main.js" => "1f8ea5f2187f0fd985b1e359baf7a5572d484fe4",
-	"main.css" => "ecfa58c396dedae718b864ac6fa71c0fdb459e21",
+	"main.css" => "fb0eb7a5c88ec3c11e7ab0caac539eaef8d556ae",
 );
