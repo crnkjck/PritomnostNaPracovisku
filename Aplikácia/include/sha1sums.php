e4344e74be5e6c5f85b9a4e635f9131cb552a43f
Ján Kľuka <jan.kluka@fmph.uniba.sk>
2021-01-25 14:37:55 +0100

    Doladenie vzhladu tlacidla na stiahnutie vykazu

--- Aplikácia/include/sha1sums.php
+++ Aplikácia/include/sha1sums.php
@@ -1,4 +1,4 @@
 <?php $sha1sums = array(
 	"main.js" => "1f8ea5f2187f0fd985b1e359baf7a5572d484fe4",
-	"main.css" => "ad45fb4b2c75bb316f934a1f5b085a776ceee655",
+	"main.css" => "4ed19a169cca0cf4fc941f8fea3fea68b642d725",
 );
